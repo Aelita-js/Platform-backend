449595b8a006850a1cc04a385e2abd29aad83e0a
Aelita-js <aiko.imanalieva00@gmail.com>
2022-01-04 22:51:36 +0600

    added script

--- index.js
+++ index.js
@@ -7,13 +7,17 @@ const router = require('./routes/index.js');
 const errorMiddleware = require('./middlewares/error-middleware.js');
 
 const PORT = process.env.PORT || 5000;
-const app = express()
+const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors());
 app.use('/api', router);
 app.use(errorMiddleware);
+
+app.get('/', (req, res) => {
+    res.end('<h1>Hello! This is Online course platform backend</h1>')
+});
 
 const start = async () => {
     try {
